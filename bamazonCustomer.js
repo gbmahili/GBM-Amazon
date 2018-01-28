6644839fce4197bd96adbff0b32307f902f9424d
gbmahili <georgesmahili@gmail.com>
2018-01-28 11:39:16 -0500

    Added files to the database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,71 +2,40 @@
 require("dotenv").config();
 // Let's get the password module
 var mysql_credentials = require("./keys.js");
-
-// Create a connection
 // DB Password
 db_password = mysql_credentials.db_password;
+// Create a connection
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+    host: '127.0.0.1',
+    port : "3306",
+    user: 'root',
+    password: db_password,
+    database: 'bamazon'
+});
 
-// INSERT INTO products SET ?
-//     {
-//         product_name: "Galaxy S8",
-//         department_name: "Electronics",
-//         price: 800.99,
-//         stock_quantity: 100
-//     },
-//     {
-//         product_name: "Samsung Monitor",
-//         department_name: "Electronics",
-//         price: 199.99,
-//         stock_quantity: 50
-//     },
-//     {
-//         product_name: "Masala Chai",
-//         department_name: "Grocery",
-//         price: 3.99,
-//         stock_quantity: 500
-//     },
-//     {
-//         product_name: "Eloquant JavaScript",
-//         department_name: "Education",
-//         price: 19.99,
-//         stock_quantity: 6
-//     },
-//     {
-//         product_name: "PRPS Goods & Co. Men's Demon Slim Fit Jean",
-//         department_name: "Men's Clothing",
-//         price: 258.00,
-//         stock_quantity: 1
-//     },
-//     {
-//         product_name: "Fc Barcelona Set Beanie Pom Pom Skull Cap Hat and Scarf Reversible New Season",
-//         department_name: "Sport's Fun Shop",
-//         price: 27.99,
-//         stock_quantity: 11
-//     },
-//     {
-//         product_name: "King Arthur Flour 100% Organic Bread Flour , 2 Pound (Pack of 12)",
-//         department_name: "Grocery",
-//         price: 56.18,
-//         stock_quantity: 4
-//     },
-//     {
-//         product_name: "Beigel's Braided Challahs - Pack of 2 Challahs",
-//         department_name: "Grocery",
-//         price: 18.98,
-//         stock_quantity: 58
-//     },
-//     {
-//         product_name: "Understanding JavaScript",
-//         department_name: "Education",
-//         price: 25.99,
-//         stock_quantity: 9
-//     },
-//     {
-//         product_name: "Beats Solo3 Wireless On-Ear Headphones - Neighborhood Collection - Asphalt Gray",
-//         department_name: "Electronics",
-//         price: 299.95,
-//         stock_quantity: 1000
-//     }
+// Connect to DB
+connection.connect();
+// These are the items we inserted in the DB:
+// var valuesToInstert = `("Galaxy S8", "Electronics", 800.99, 100),
+//     ("Samsung Monitor", "Electronics", 199.99, 50),
+//     ("Masala Chai", "Grocery", 3.99, 500),
+//     ("Eloquant JavaScript", "Education", 19.99, 6),
+//     ("PRPS Goods & Co. Men's Demon Slim Fit Jean", "Men's Clothing", 258.00, 1),
+//     ("Fc Barcelona Set Beanie Pom Pom Skull Cap Hat and Scarf Reversible New Season", "Sport's Fun Shop", 27.99, 11),
+//     ("King Arthur Flour 100% Organic Bread Flour , 2 Pound (Pack of 12)", "Grocery", 56.18, 4),
+//     ("Beigel's Braided Challahs - Pack of 2 Challahs", "Grocery", 18.98, 58),
+//     ("Understanding JavaScript", "Education", 25.99, 9),
+//     ("Beats Solo3 Wireless On-Ear Headphones - Neighborhood Collection - Asphalt Gray", "Electronics", 299.95, 1000)`;
 
-// SELECT * FROM products;
+// Running this application will first display all of the items available for sale. 
+// Include the ids, names, and prices of products for sale.
+
+// Query the DB
+connection.query(`SELECT item_id, product_name, price FROM products`, function (error, results, fields) {
+    if (error) throw error;
+    // console.log(results); 
+       
+});
+// End the connection
+connection.end();
