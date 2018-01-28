01a0ab42a2e5ecd9e8c2a696d0a4d8a42ccec31e
gbmahili <georgesmahili@gmail.com>
2018-01-28 13:44:45 -0500

    Added inquirer to ask the customer for what they want to buy

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,6 +23,9 @@ var columnify = require('columnify');
 connection.query(`SELECT item_id, product_name, price FROM products`, function (error, results, fields) {
     if (error) throw error;
     // Let's use columnify
+    console.log("| ------------------------------------------------------------------------------------------------|");
+    console.log("|    Hi there! Welcome to Mahili's Amazon-like store. Here is a list of available items:          |");
+    console.log("| ------------------------------------------------------------------------------------------------|");
     console.log(columnify(results, {columnSplitter: ' | '}));
     // // Log the row header
     // console.log(" | Item ID | Product Name | Price |");
@@ -35,7 +38,42 @@ connection.query(`SELECT item_id, product_name, price FROM products`, function (
     //     }else{
     //         console.log(` |   ${element.item_id}    | ${element.product_name} | ${element.price} |`);
     //     };
-    // });       
+    // }); 
+    // Ask the user what they want to buy and add them to the cart:
+    addToCart();      
 });
 // End the connection
 connection.end();
+
+// Le'ts prompt the user to chose an item and add them to the cart:
+var addToCart = function () {
+    var inquirer = require("inquirer");
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "item_id",
+            message: "What is the ID of the product you would like to buy?",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "How many units of the product would would like to buy?",
+            validate: function (value) {
+                if (isNaN(value) === false) {
+                    return true;
+                }
+                return false;
+            }
+        }
+    ]).then(function(answers){
+        console.log(`You would like to buy
+        ITEM_ID: ${answers.item_id}
+        QUANTITY: ${answers.quantity}`);
+    });
+};
