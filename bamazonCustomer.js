071498fe8120ed81ef802fd307be6a26ab965660
gbmahili <georgesmahili@gmail.com>
2018-01-28 13:13:38 -0500

    Added columnify from the npm to make columns the same size

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,29 +13,29 @@ var connection = mysql.createConnection({
     password: db_password,
     database: 'bamazon'
 });
-
 // Connect to DB
 connection.connect();
-// These are the items we inserted in the DB:
-// var valuesToInstert = `("Galaxy S8", "Electronics", 800.99, 100),
-//     ("Samsung Monitor", "Electronics", 199.99, 50),
-//     ("Masala Chai", "Grocery", 3.99, 500),
-//     ("Eloquant JavaScript", "Education", 19.99, 6),
-//     ("PRPS Goods & Co. Men's Demon Slim Fit Jean", "Men's Clothing", 258.00, 1),
-//     ("Fc Barcelona Set Beanie Pom Pom Skull Cap Hat and Scarf Reversible New Season", "Sport's Fun Shop", 27.99, 11),
-//     ("King Arthur Flour 100% Organic Bread Flour , 2 Pound (Pack of 12)", "Grocery", 56.18, 4),
-//     ("Beigel's Braided Challahs - Pack of 2 Challahs", "Grocery", 18.98, 58),
-//     ("Understanding JavaScript", "Education", 25.99, 9),
-//     ("Beats Solo3 Wireless On-Ear Headphones - Neighborhood Collection - Asphalt Gray", "Electronics", 299.95, 1000)`;
-
 // Running this application will first display all of the items available for sale. 
 // Include the ids, names, and prices of products for sale.
-
 // Query the DB
+// Use the colunbify package to make columns the ssame sile in the terminal: (check the version in the package.json)
+var columnify = require('columnify');
 connection.query(`SELECT item_id, product_name, price FROM products`, function (error, results, fields) {
     if (error) throw error;
-    // console.log(results); 
-       
+    // Let's use columnify
+    console.log(columnify(results, {columnSplitter: ' | '}));
+    // // Log the row header
+    // console.log(" | Item ID | Product Name | Price |");
+    // console.log(" | ------- | ------------ | ----- |");
+    // // Loop through the data and display it
+    // results.forEach(element => {
+    //     if (element.item_id < 10){
+    //         element.item_id = `0${element.item_id}`;
+    //         console.log(` |   ${element.item_id}    | ${element.product_name} | ${element.price} |`);
+    //     }else{
+    //         console.log(` |   ${element.item_id}    | ${element.product_name} | ${element.price} |`);
+    //     };
+    // });       
 });
 // End the connection
 connection.end();
